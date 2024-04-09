2cc7c176ab59322dd7f765fb31f595441b3e48b3
mnbd <mnbdpro@gmail.com>
2024-04-09 10:05:57 +0200

    deps: mongoose

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import express from "express";
 import carsRoutes from "./routes/cars.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 console.log("env: ", process.env.MONGO_STRING);
 
 const PORT = process.env.PORT || 3001;
+const MONGO_STRING = process.env.MONGO_STRING;
 
 // Middlewares
 // Middleware qui permet de parser les donnees issues d'un formulaire
@@ -14,18 +16,11 @@ app.use(express.json());
 
 app.use("/cars", carsRoutes);
 
-/* 
- Routes definit une route Get sur / qui renvoie un message
- La fonction app.get prend deux arguments
- le chemin de la route
- et une fonction qui prend deux arguments :
- request et response 
- */
-app.get("/", (request, response) => {
-  response.json({ message: "Hello World!" });
-  // JSON : Javascript Object Notation
-});
+mongoose.connect(MONGO_STRING).then(() => {
+  console.log("Connected to the database!");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  // On lance le serveur
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
