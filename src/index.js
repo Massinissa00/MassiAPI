04f0c8938d8dde7b65a40f73f436358ff0c29665
mnbd <mnbdpro@gmail.com>
2024-04-09 15:18:17 +0200

    feat: auth flow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import express from "express";
 import carsRoutes from "./routes/cars.js";
+import authRoutes from "./routes/auth.js";
 import mongoose from "mongoose";
 import { handleUncaughtErrors } from "./Middlewares/error.js";
+import isAuth from "./Middlewares/auth.js";
 
 const app = express();
 
@@ -13,7 +15,8 @@ const MONGO_STRING = process.env.MONGO_STRING;
 // Middleware qui permet de parser les donnees issues d'un formulaire
 app.use(express.json());
 
-app.use("/cars", carsRoutes);
+app.use("/cars", isAuth, carsRoutes);
+app.use("/auth", authRoutes);
 
 app.use("/error", (req, res) => {
   try {
