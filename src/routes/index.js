42c360999df96f1f0dab84d8c46543a1962c19a7
mnbd <mnbdpro@gmail.com>
2024-04-10 16:41:41 +0200

    feat: image upload route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 
 import authRouter from "./auth.js";
 import carsRouter from "./cars.js";
+import fileRouter from "./file.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 
 router.use("/cars", carsRouter);
+
+router.use("/uploads", fileRouter);
 
 export default router;
